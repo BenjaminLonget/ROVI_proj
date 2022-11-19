e9154054cf95937d5da50a69ce71f3055f33ab8b
Benjamin Longet <benjamin_longet@hotmail.com>
2022-11-19 13:55:02 +0100

    PointCloudGen er tilføjet. Text fil i mappen beskriver hvordan man gør, ellers burde det være nok med pcd filen

---
+++ PointCloudGen/build/CMakeFiles/pcd_gen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_gen.dir/src/pcd_gen.cpp.o"
+  "CMakeFiles/pcd_gen.dir/src/pcd_gen.cpp.o.d"
+  "pcd_gen"
+  "pcd_gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
